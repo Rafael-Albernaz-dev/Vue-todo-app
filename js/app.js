7cb95b9b6a584c9ff27ca0499ad29c9bcbd833db
Rafael-Albernaz-dev <rafaelalbernaz6@gmail.com>
2020-01-18 00:52:27 -0300

    add js FOLDER

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Tasks',
+        newTodo: '',
+        todos: []
+    },
+
+
+
+    methods: {
+        addTodo() {
+            this.todos.push({
+                title: this.newTodo,
+                done: false,
+            });
+            this.newTodo = '';
+            this.saveTodos();
+        },
+
+        removeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex,1);
+            this.saveTodos();
+        },
+
+        allDone() {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            })
+            this.saveTodos();
+        },
+
+        allDelete() {
+            this.todos = [];
+            this.saveTodos();
+        },
+
+        saveTodos() {
+            const parsed = JSON.stringify(this.todos);
+            localStorage.setItem('todos',parsed);
+        },
+
+
+    },
+
+        
+    mounted() {
+        if(localStorage.getItem('todos')) {
+            try {
+                this.todos = JSON.parse(localStorage.getItem('todos'));
+            } catch(e) {
+                localStorage.removeItem('todos');
+            }
+        }
+    },
+
+});
+
